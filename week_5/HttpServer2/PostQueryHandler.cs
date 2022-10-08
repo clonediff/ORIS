23c95970bf1321634a4c475798a2bfc07bacddb0
clonediff <32036592+clonediff@users.noreply.github.com>
2022-10-08 11:47:49 +0300

    [add] HttpServer2

---
+++ week_5/HttpServer2/PostQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer2
+{
+    class PostQueryHandler
+    {
+        public static void HandlePostQuery(HttpListenerContext context, ServerSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ week_5/HttpServer2/ServerSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace HttpServer2
+{
+    public static class Constants
+    {
+        public const string NotFoundPage =
+@"<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset=""utf-8"" />
+        <title>404 Not Found</title>
+    </head>
+    <body style=""display:flex; justify-content:center;"">
+        <h1>404 Not Found</h1>
+    </body>
+</html>";
+    }
+
+    public class ServerSettings
+    {
+        public int Port { get; init; }
+        public PageSetting Google { get; init; }
+        public PageSetting Steam { get; init; }
+        public PageSetting MainPage { get; init;  }
+    }
+
+    public class PageSetting
+    {
+        public string RawUrl { get; init; }
+        public string MainFolder { get; init; }
+        public string MainHTML { get; init; }
+    }
+}
