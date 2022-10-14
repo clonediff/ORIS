4019616ac915fa1c1438a6c1659159cd5d578881
clonediff <32036592+clonediff@users.noreply.github.com>
2022-10-14 17:41:27 +0300

    [add] week_5.2

---
+++ week_5.2/HttpServer2/GetQueryHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer2
+{
+    class GetQueryHandler
+    {
+        ServerSettings _settings;
+        public GetQueryHandler(ServerSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public void HandleGetQuery(HttpListenerContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            var filePath = GetFilePath(request.UrlReferrer?.LocalPath, request.RawUrl!);
+
+            byte[] buffer;
+            if (File.Exists(filePath))
+            {
+                buffer = File.ReadAllBytes(filePath);
+                response.ContentType = ContentTypeIdentifier.GetContentType(filePath);
+            } else
+            {
+                buffer = Encoding.UTF8.GetBytes(Constants.NotFoundPage);
+                response.ContentType = "text/html; charset=UTF-8";
+                response.StatusCode = 404;
+            }
+            response.ContentLength64 = buffer.Length;
+
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+
+        public string GetFilePath(string? referrerLocalPath, string rawUrl)
+        {
+            var filePath = "";
+            if (string.IsNullOrEmpty(referrerLocalPath))
+            {
+                if (rawUrl == _settings.Google.RawUrl)
+                    filePath = Path.Combine(_settings.Google.MainFolder, _settings.Google.MainHTML);
+                else if (rawUrl == _settings.Steam.RawUrl)
+                    filePath = Path.Combine(_settings.Steam.MainFolder, _settings.Steam.MainHTML);
+                else if (rawUrl == _settings.MainPage.RawUrl)
+                    filePath = Path.Combine(_settings.MainPage.MainFolder, _settings.Steam.MainHTML);
+
+            }
+            else if (referrerLocalPath == _settings.Google.RawUrl)
+            {
+                filePath = Path.Combine(_settings.Google.MainFolder, rawUrl.Substring(1));
+            }
+            else if (referrerLocalPath == _settings.Steam.RawUrl)
+            {
+                filePath = Path.Combine(_settings.Steam.MainFolder, rawUrl.Substring(1));
+            }
+            return filePath;
+        }
+    }
+}
---
+++ week_5.2/HttpServer2/HttpServer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpServer2;
+
+public class HttpServer : IDisposable
+{
+    internal HttpListener _listener;
+    public ServerStatus Status = ServerStatus.Stop;
+    private ServerSettings _settings;
+
+    public HttpServer()
+    {
+        _listener = new();
+    }
+
+    public void Start()
+    {
+        if (Status == ServerStatus.Start)
+        {
+            Console.WriteLine("Сервер уже запущен");
+            return;
+        }
+
+        var settingsPath = "settings.json";
+        if (File.Exists(settingsPath))
+            _settings = JsonSerializer.Deserialize<ServerSettings>(File.ReadAllText(settingsPath));
+        else
+        {
+            Console.WriteLine("Не найден файл настроек. Невозможно запустить сервер");
+            return;
+        }
+
+        _listener.Prefixes.Clear();
+        _listener.Prefixes.Add($"http://localhost:{_settings.Port}/");
+        Console.WriteLine("Запуск сервера...");
+        _listener.Start();
+        Status = ServerStatus.Start;
+        Console.WriteLine("Сервер запущен");
+
+        ListeningAsync();
+    }
+
+    public void Stop()
+    {
+        if (Status == ServerStatus.Stop) return;
+
+        Console.WriteLine("Остановка сервера...");
+        _listener.Stop();
+        Status = ServerStatus.Stop;
+        Console.WriteLine("Сервер остановлен");
+    }
+
+    public void Restart()
+    {
+        if (Status == ServerStatus.Stop)
+        {
+            Console.WriteLine("Сервер уже остановлен. Для запуска используйте start вместо restart");
+            return;
+        }
+
+        //Console.WriteLine("Перезапуск сервера...");
+        Stop();
+        Start();
+        //Console.WriteLine("Сервер перезапущен");
+    }
+
+    private async Task ListeningAsync()
+    {
+        while (Status == ServerStatus.Start)
+        {
+            var context = await _listener.GetContextAsync();
+            switch (context.Request.HttpMethod)
+            {
+                case "GET":
+                    new GetQueryHandler(_settings).HandleGetQuery(context);
+                    break;
+                case "POST":
+                    PostQueryHandler.HandlePostQuery(context, _settings);
+                    break;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        Stop();
+    }
+}
