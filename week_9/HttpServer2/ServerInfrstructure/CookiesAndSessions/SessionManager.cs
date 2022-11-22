0796071284e6b10381f8e33b1acd4c99af6497d4
clonediff <32036592+clonediff@users.noreply.github.com>
2022-11-22 13:37:33 +0300

    [fix] Commit 5

--- week_9/HttpServer2/ServerInfrstructure/CookiesAndSessions/SessionManager.cs
+++ week_9/HttpServer2/ServerInfrstructure/CookiesAndSessions/SessionManager.cs
@@ -22,7 +22,8 @@ namespace HttpServer2.ServerInfrstructure.CookiesAndSessions
             if (createdTime == default)
                 createdTime = DateTime.Now;
             var session = new Session { Id = guid, AccountId = accountId, Login = login, CreateDateTime = createdTime };
-            _chache.Set(guid, session);
+            _chache.Set(guid, session,
+                new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(2)));
             return session;
         }
 
